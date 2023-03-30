bb6343795bf4423a4e2c704f461472f96f972a52
kowalski <kamarulnizam010617@gmail.com>
2023-03-30 15:55:09 +0800

    activity week 2

---
+++ hajim.js
@@ -0,0 +1,35 @@
+
+
+//let str = 69.05
+//let num = 69.211
+
+//let total = parseFloat(str)/num
+//console.log(total,typeof total)
+let data={
+    name:"KK",
+    age:22,
+    faculty:"FKEKK",
+}
+console.log(data.name)
+console.log(data.age)
+console.log(data.faculty)
+
+let random=["utv",1701,false]
+console.log(random[0],random[1],random[2])
+
+let message = [
+    {
+        user: "usr1",
+        date: "27/11/2023",
+        msg: "amazing",
+        likes: 23,
+    },
+
+    {
+        user: "usr2",
+        date:"30/11/2023",
+        msg: "nice one",
+        likes: 33
+    },
+]
+console.log(message[1].likes)
